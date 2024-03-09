ff50ab79a5b422641a1a23579737759728357c09
FelipeSantos-cco <felipe.almeida@sptech.school>
2024-03-09 00:47:03 -0300

    :bug: Fix: Remoção de nome inválido e sobrecarga

--- poc-bobble-sort/src/main/java/school/sptech/BubbleSort.java
+++ poc-bobble-sort/src/main/java/school/sptech/BubbleSort.java
@@ -20,16 +20,15 @@ public class BubbleSort {
         return v;
     }
 
-    public static double[] ordenaVetorInteiro(double[] v){
+    public static double[] ordenaVetorDouble(double[] v){
         double numeroTemp;
 
         for (int i = 0; i < v.length; i++) {
-            for (int j = 0; j < v.length - i - 1; j++) {
-                if(v[j] > v[j+1]){
-
-                    numeroTemp = v[j];
-                    v[j] = v[j+1];
-                    v[j+1] = numeroTemp;
+            for (int y = 0; y < v.length - i - 1; y++) {
+                if(v[y] > v[y+1]){
+                    numeroTemp = v[y];
+                    v[y] = v[y+1];
+                    v[y+1] = numeroTemp;
                 }
             }
         }
