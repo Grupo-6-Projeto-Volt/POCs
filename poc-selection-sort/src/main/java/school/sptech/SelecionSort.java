244e0a5b75b0dac78da7c6cc89b7311f12a460d3
FelipeSantos-cco <felipe.almeida@sptech.school>
2024-03-09 15:28:55 -0300

    :sparkles: Feat: Ordenação de Vetores inteiros

--- poc-selection-sort/src/main/java/school/sptech/SelecionSort.java
+++ poc-selection-sort/src/main/java/school/sptech/SelecionSort.java
@@ -1,4 +1,24 @@
 package school.sptech;
 
 public class SelecionSort {
+
+    public static void selectionSort(int[] v){
+        int temp = 0;
+        int indMenor = 0;
+
+        long tempoInicial = System.currentTimeMillis();
+        for (int i = 0; i < v.length - 1; i++) {
+            indMenor = i;
+            for (int j = i+1; j < v.length; j++) {
+                if(v[j] < v[indMenor]){
+                    indMenor = j;
+                }
+            }
+            temp = v[i];
+            v[i] = v[indMenor];
+            v[indMenor] = temp;
+        }
+        long tempoFinal = System.currentTimeMillis() - tempoInicial;
+        System.out.println(("Tempo de execussão: %d ms").formatted(tempoFinal));
+    }
 }
