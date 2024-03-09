31acde72ff97876487a3f68b284fd749f23b67e7
FelipeSantos-cco <felipe.almeida@sptech.school>
2024-03-09 00:33:22 -0300

    :sparkles: Feat: Ordenando vetor de inteiros

--- poc-bobble-sort/src/main/java/school/sptech/BubbleSort.java
+++ poc-bobble-sort/src/main/java/school/sptech/BubbleSort.java
@@ -1,4 +1,24 @@
 package school.sptech;
 
 public class BubbleSort {
+
+    public static int[] ordenaVetorInteiro(int[] v){
+        int numeroTemp;
+
+        for (int i = 0; i < v.length; i++) {
+            for (int j = 0; j < v.length - i - 1; j++) {
+
+                // O numero de agora(v[j]) é maior que o proximo (v[j + 1])?
+                if(v[j] > v[j+1]){
+
+                    numeroTemp = v[j];
+                    v[j] = v[j+1];
+                    v[j+1] = numeroTemp;
+                }
+            }
+        }
+        return v;
+    }
+
+    
 }
